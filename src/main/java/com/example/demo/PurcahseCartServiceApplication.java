244d752aee4bd00dd75ef78dbaadb1ecc1200c05
Evandro <e.maddes@reply.it>
2025-02-03 18:30:49 +0100

    init repo using spring initializer

---
+++ src/main/java/com/example/demo/PurcahseCartServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurcahseCartServiceApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurcahseCartServiceApplication.class, args);
+    }
+
+}
