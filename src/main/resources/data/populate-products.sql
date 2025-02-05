135c1b6dd2af86d28670a0d8908c7acc1fa7507f
Evandro <e.maddes@reply.it>
2025-02-05 14:55:21 +0100

    add sql scripts to populate db

---
+++ src/main/resources/data/populate-products.sql
@@ -0,0 +1,12 @@
+INSERT INTO product(id, price_value, vat_value)
+VALUES (10, 5.00, 0.70);
+
+INSERT INTO warehouse(id, quantity, product_id)
+VALUES (10, 3, 10);
+
+
+INSERT INTO product(id, price_value, vat_value)
+VALUES (11, 3.00, 0.22);
+
+INSERT INTO warehouse(id, quantity, product_id)
+VALUES (11, 5, 11);
---
+++ src/test/resources/data/populate-products.sql
@@ -0,0 +1,12 @@
+INSERT INTO product(id, price_value, vat_value)
+VALUES (12, 5.00, 0.70);
+
+INSERT INTO warehouse(id, quantity, product_id)
+VALUES (12, 3, 12);
+
+
+INSERT INTO product(id, price_value, vat_value)
+VALUES (13, 3.00, 0.22);
+
+INSERT INTO warehouse(id, quantity, product_id)
+VALUES (13, 5, 13);
