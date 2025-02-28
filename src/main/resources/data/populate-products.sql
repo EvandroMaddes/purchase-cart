2770e46007f111ce5b89c4f5c9250d98d78503f2
evandro <maddesevandro@gmail.com>
2025-02-28 09:50:06 +0100

    set data like example

--- src/main/resources/data/populate-products.sql
+++ src/main/resources/data/populate-products.sql
@@ -4,7 +4,10 @@ VALUES (1, 'tax free', 0.0),
        (3, 'default vat', 0.70);
 
 INSERT INTO product(id, unit_price, available_quantity, vat_rate_id)
-VALUES (10, 5.00, 3, 3);
+VALUES (1, 5.00, 3, 3);
 
 INSERT INTO product(id, unit_price, available_quantity, vat_rate_id)
-VALUES (11, 3.00, 5, 2);
+VALUES (2, 3.00, 5, 2);
+
+INSERT INTO product(id, unit_price, available_quantity, vat_rate_id)
+VALUES (3, 12.00, 4, 3);
